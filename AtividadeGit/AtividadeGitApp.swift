34dda8293cf588bfc9490b97d615b55d9eb6868a
litaR84 <thalitarbullos@gmail.com>
2022-10-11 15:07:11 -0300

    Initial Commit

---
+++ AtividadeGit/AtividadeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeGitApp.swift
+//  AtividadeGit
+//
+//  Created by Thalita Bullos on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
